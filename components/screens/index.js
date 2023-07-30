6f58159b6d9c1a8512f294406a96f08a3b83d424
Muhamad Yazid Imani <imaniyazid@gmail.com>
2023-07-30 09:16:41 +0700

    feat: Add new screen and components

---
+++ components/screens/index.js
@@ -0,0 +1,14 @@
+import Home from './Home';
+import Packages from './Packages';
+import About from './About';
+import LoadingScreen from './LoadingScreen';
+import WebviewPackage from './WebView/WebviewPackage'
+import WebviewAbout from './WebView/WebviewAbout'
+export {
+	Home,
+	Packages,
+	About,
+	LoadingScreen,
+	WebviewPackage,
+	WebviewAbout
+}
