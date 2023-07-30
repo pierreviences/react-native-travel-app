61cabbd8c9b923f214dbce3e23191a9ab8ccf417
Muhamad Yazid Imani <imaniyazid@gmail.com>
2023-07-30 09:07:34 +0700

    feat: Set up React Native project and install dependencies

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View} from 'react-native';  
+import App from './components/navigations/Main';
+import {Root} from 'native-base';
+
+function Apps() {
+  return (
+  	<Root>
+      
+    <App />
+    </Root>
+    )
+}
+export default Apps;
